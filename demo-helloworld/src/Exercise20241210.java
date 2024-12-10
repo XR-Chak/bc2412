f707048a38ddd895c0a23371bd4ee7e4b8671133
XR_Chak <kwchak3-c@my.cityu.edu.hk>
2024-12-10 12:55:26 +0800

    exercise

--- demo-helloworld/src/Exercise20241210.java
+++ demo-helloworld/src/Exercise20241210.java
@@ -7,5 +7,12 @@ public class Exercise20241210{
     int quantityOfOrange = 4;
     double totalAmount = priceOfApple*quantityOfApple+priceOfOrange*quantityOfOrange;
     System.out.println("totalAmount: "+totalAmount);
+
+    int mathScore = 73;
+    int engScore = 60;
+    int historyScore = 61;
+    double averageScore = (mathScore+engScore+historyScore)/3.0;
+    System.out.println("averageScore is: "+averageScore);
+
   }
 }
